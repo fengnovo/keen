3b383dd6c09c6dc0b493e7eff9b6b04c784e7628
keenhuang <keenhuang@tencent.com>
2022-03-18 21:47:42 +0800

    feat:  vercel serverless

---
+++ api/index.ts
@@ -0,0 +1,50 @@
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import got, { Response } from 'got'
+
+interface Repository {
+  name: string
+  full_name: string
+  description: string
+  updated_at: string
+  owner: { login: string }
+}
+
+interface Template {
+  name: string
+  owner: string
+  fullname: string
+  description: string
+  updated: string
+}
+
+const fetchAllRepos = (owner: string): AsyncIterableIterator<Repository> => {
+  console.log('url---', `https://api.github.com/users/${owner}/repos?type=owner&sort=updated&per_page=100`);
+  return got.paginate<Repository>(`https://api.github.com/users/${owner}/repos?type=owner&sort=updated&per_page=100`, {
+    timeout: 5 * 1000,
+    username: process.env.GITHUB_CLIENT_ID,
+    password: process.env.GITHUB_CLIENT_SECRET
+  })
+}
+
+export default async (req: VercelRequest, res: VercelResponse): Promise<void> => {
+  const owner = req.query.owner ?? req.query.username ?? 'caz-templates'
+  console.log('req.query.owner---', req.query.owner);
+  console.log('req.query.username---', req.query.username);
+  console.log('owner---', owner);
+  try {
+    const results: Template[] = []
+    for await (const item of fetchAllRepos(owner.toString())) {
+      results.push({
+        name: item.name,
+        owner: item.owner.login,
+        fullname: item.full_name,
+        description: item.description,
+        updated: item.updated_at
+      })
+    }
+    res.json(results)
+  } catch (e) {
+    const response = e.response as Response
+    res.status(response.statusCode).json(response.body)
+  }
+}
